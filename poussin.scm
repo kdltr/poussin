0af16237730477be0d100d62c557ae732000e154
Kooda <kooda@upyum.com>
2018-12-26 13:06:08 +0100

    Move the tests into their own directory

--- poussin.scm
+++ poussin.scm
@@ -1,8 +1,7 @@
 ;; TODO cyclic list handling everywhere
 
 (import (chicken read-syntax)
-        srfi-1
-        test)
+        srfi-1)
 
 (define (kernel-eval exp env)
   (cond ((symbol? exp)
@@ -162,26 +161,26 @@
     (lambda (operand-tree env)
       (applicative-combiner (car operand-tree)))))
 
-(define core-environment (make-environment `((boolean? . ,foreign-boolean?)
-                                             (equal? . ,foreign-equal?)
-                                             (symbol? . ,foreign-symbol?)
-                                             (inert? . ,foreign-inert?)
-                                             ($if . ,foreign-$if)
-                                             (pair? . ,foreign-pair?)
-                                             (null? . ,foreign-null?)
-                                             (cons . ,foreign-cons)
-                                             (environment? . ,foreign-environment?)
-                                             (ignore? . ,foreign-ignore?)
-                                             (eval . ,foreign-eval)
-                                             (make-environment . ,foreign-make-environment)
-                                             ($define! . ,foreign-$define!)
-                                             (operative? . ,foreign-operative?*)
-                                             (applicative? . ,foreign-applicative?)
-                                             ($vau . ,foreign-$vau)
-                                             (wrap . ,foreign-wrap)
-                                             (unwrap . ,foreign-unwrap)
-                                             )
-                                           '()))
+(define core-environment
+  (make-environment `((boolean? . ,foreign-boolean?)
+                      (equal? . ,foreign-equal?)
+                      (symbol? . ,foreign-symbol?)
+                      (inert? . ,foreign-inert?)
+                      ($if . ,foreign-$if)
+                      (pair? . ,foreign-pair?)
+                      (null? . ,foreign-null?)
+                      (cons . ,foreign-cons)
+                      (environment? . ,foreign-environment?)
+                      (ignore? . ,foreign-ignore?)
+                      (eval . ,foreign-eval)
+                      (make-environment . ,foreign-make-environment)
+                      ($define! . ,foreign-$define!)
+                      (operative? . ,foreign-operative?*)
+                      (applicative? . ,foreign-applicative?)
+                      ($vau . ,foreign-$vau)
+                      (wrap . ,foreign-wrap)
+                      (unwrap . ,foreign-unwrap))
+                    '()))
 
 (define standard-environment (make-environment '() (list core-environment)))
 
@@ -192,65 +191,3 @@
         (write (kernel-eval exp standard-environment))
         (newline)
         (kernel-repl)))))
-
-
-;; =====
-;; TESTS
-;; =====
-
-(test-group "parameter matching"
-
-(test "matching ignore" '() (match-formal-parameter-tree #!ignore 42 '()))
-(test "matching symbol" '((a . 42)) (match-formal-parameter-tree 'a 42 '()))
-(test "matching nil" '() (match-formal-parameter-tree '() '() '()))
-(test-error "nil matching error" (match-formal-parameter-tree '() 42 '()))
-(test "matching pair" '((a . 42) (b . 21)) (match-formal-parameter-tree '(a . b) '(42 . 21) '()))
-(test "matching list" '((a . 42) (b . 21)) (match-formal-parameter-tree '(a b) '(42 21) '()))
-(test-error "duplicate symbol error" (match-formal-parameter '(a (a b)) '(1 (2 3)) '()))
-
-) ; parameter matching group
-
-(test-group "eval"
-
-(define empty-environment (make-environment '() '()))
-
-(test "evaluating number" 42 (kernel-eval 42 empty-environment))
-(test "evaluating strings" "foo" (kernel-eval "foo" empty-environment))
-(test "evaluating #t" #t (kernel-eval #t empty-environment))
-(test "evaluating #f" #f (kernel-eval #f empty-environment))
-
-(define simple-environment (make-environment `((foo . 42)
-                                               (a . 1)
-                                               (b . 2)
-                                               (c . 3))
-                                             '()))
-(define simple-child-environment (make-environment '() (list simple-environment)))
-
-(test "evaluating bound symbol" 42 (kernel-eval 'foo simple-environment))
-(test-error "evaluating unbound symbol" (kernel-eval 'bar simple-environment))
-(test "evaluating bound symbol in parent environment" 42 (kernel-eval 'foo simple-child-environment))
-
-(define simple-operative (make-operative 'x 'e 'x empty-environment))
-(define simple-applicative (make-applicative simple-operative))
-(define combiner-environment (make-environment (list (cons '$list simple-operative)
-                                                     (cons 'list simple-applicative))
-                                               (list simple-environment)))
-
-(test "evaluating simple operative" '(a b c) (kernel-eval '($list a b c) combiner-environment))
-(test "evaluating simple applicative" '(1 2 3) (kernel-eval '(list a b c) combiner-environment))
-
-
-(test-group "foreign combiners"
-
-(define foreign-environment (make-environment (list (cons '$vau foreign-$vau)
-                                                    (cons 'wrap foreign-wrap))
-                                              (list combiner-environment)))
-
-(test "foreign wrap call" '(1 2 3) (kernel-eval '((wrap $list) a b c) foreign-environment))
-(test "foreign $vau call" '(b a) (kernel-eval '(($vau (x y) #!ignore (list y x)) a b) foreign-environment))
-
-) ; foreign combiners group
-
-) ; eval group
-
-(cond-expand (script (test-exit)) (else))
---
+++ tests/run.scm
@@ -0,0 +1,60 @@
+(import (chicken load) test)
+
+(load-relative "../poussin.scm")
+
+(test-group "parameter matching"
+
+(test "matching ignore" '() (match-formal-parameter-tree #!ignore 42 '()))
+(test "matching symbol" '((a . 42)) (match-formal-parameter-tree 'a 42 '()))
+(test "matching nil" '() (match-formal-parameter-tree '() '() '()))
+(test-error "nil matching error" (match-formal-parameter-tree '() 42 '()))
+(test "matching pair" '((a . 42) (b . 21)) (match-formal-parameter-tree '(a . b) '(42 . 21) '()))
+(test "matching list" '((a . 42) (b . 21)) (match-formal-parameter-tree '(a b) '(42 21) '()))
+(test-error "duplicate symbol error" (match-formal-parameter '(a (a b)) '(1 (2 3)) '()))
+
+) ; parameter matching group
+
+(test-group "eval"
+
+(define empty-environment (make-environment '() '()))
+
+(test "evaluating number" 42 (kernel-eval 42 empty-environment))
+(test "evaluating strings" "foo" (kernel-eval "foo" empty-environment))
+(test "evaluating #t" #t (kernel-eval #t empty-environment))
+(test "evaluating #f" #f (kernel-eval #f empty-environment))
+
+(define simple-environment (make-environment `((foo . 42)
+                                               (a . 1)
+                                               (b . 2)
+                                               (c . 3))
+                                             '()))
+(define simple-child-environment (make-environment '() (list simple-environment)))
+
+(test "evaluating bound symbol" 42 (kernel-eval 'foo simple-environment))
+(test-error "evaluating unbound symbol" (kernel-eval 'bar simple-environment))
+(test "evaluating bound symbol in parent environment" 42 (kernel-eval 'foo simple-child-environment))
+
+(define simple-operative (make-operative 'x 'e 'x empty-environment))
+(define simple-applicative (make-applicative simple-operative))
+(define combiner-environment (make-environment (list (cons '$list simple-operative)
+                                                     (cons 'list simple-applicative))
+                                               (list simple-environment)))
+
+(test "evaluating simple operative" '(a b c) (kernel-eval '($list a b c) combiner-environment))
+(test "evaluating simple applicative" '(1 2 3) (kernel-eval '(list a b c) combiner-environment))
+
+
+(test-group "foreign combiners"
+
+(define foreign-environment (make-environment (list (cons '$vau foreign-$vau)
+                                                    (cons 'wrap foreign-wrap))
+                                              (list combiner-environment)))
+
+(test "foreign wrap call" '(1 2 3) (kernel-eval '((wrap $list) a b c) foreign-environment))
+(test "foreign $vau call" '(b a) (kernel-eval '(($vau (x y) #!ignore (list y x)) a b) foreign-environment))
+
+) ; foreign combiners group
+
+) ; eval group
+
+(test-exit)

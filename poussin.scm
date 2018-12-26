b86a57264be2aa98ec2fcae01993291c5db74f88
Kooda <kooda@upyum.com>
2018-12-26 11:47:53 +0100

    Expose all core features in an environment

--- poussin.scm
+++ poussin.scm
@@ -1,4 +1,7 @@
+;; TODO cyclic list handling everywhere
+
 (import (chicken read-syntax)
+        srfi-1
         test)
 
 (define (kernel-eval exp env)
@@ -14,6 +17,11 @@
 (define +ignore+ (make-ignore))
 (define-record-printer (ignore _ port) (display "#!ignore"))
 (set-read-syntax! 'ignore (lambda (_) '+ignore+))
+
+(define-record inert)
+(define +inert+ (make-inert))
+(define-record-printer (inert _ port) (display "#!inert"))
+(set-read-syntax! 'inert (lambda (_) '+inert+))
 
 (define-record environment bindings parents)
 (define-record operative formal-parameters environment-formal expression definition-environment)
@@ -66,12 +74,125 @@
 ;; FOREIGN OPERATIVES
 ;; ==================
 
-(define ($vau operand-tree static-environment)
-  (let ((formals (car operand-tree))
-        (eformal (cadr operand-tree))
-        (expr (caddr operand-tree)))
-    (void)))
-  
+;; foreign operatives are regular Scheme lambda that receive
+;; the operand-tree non-evaluated and the dynamic environment
+
+;; helper for foreign applicatives
+(define (make-foreign-applicative proc)
+  (make-applicative (make-foreign-operative proc)))
+
+;; helper for foreign predicates
+(define (make-foreign-predicate pred)
+  (make-foreign-applicative
+    (lambda (operand-tree env)
+      (every pred operand-tree))))
+
+(define foreign-boolean? (make-foreign-predicate boolean?))
+
+(define foreign-equal?
+  (make-foreign-applicative equal?))
+
+(define foreign-symbol? (make-foreign-predicate symbol?))
+
+(define foreign-inert? (make-foreign-predicate inert?))
+
+(define foreign-$if
+  (make-foreign-operative
+    (lambda (operand-tree env)
+      (let ((test (car operand-tree))
+            (consequent (cadr operand-tree))
+            (alternative (caddr operand-tree)))
+        (let ((test-result (kernel-eval test env)))
+          (unless (boolean? test-result)
+            (error "result of $if test is not a boolean" (cons '$if operand-tree)))
+          (if test-result
+              (kernel-eval consequent env)
+              (kernel-eval alternative env)))))))
+
+(define foreign-pair? (make-foreign-predicate pair?))
+(define foreign-null? (make-foreign-predicate null?))
+
+(define foreign-cons
+  (make-foreign-applicative
+    (lambda (operand-tree env)
+      (cons (car operand-tree) (cadr operand-tree)))))
+
+(define foreign-environment? (make-foreign-predicate environment?))
+(define foreign-ignore? (make-foreign-predicate ignore?))
+
+(define foreign-eval
+  (make-foreign-applicative
+    (lambda (operand-tree env)
+      (kernel-eval (car operand-tree) (cadr operand-tree)))))
+
+(define foreign-make-environment
+  (make-foreign-applicative
+    (lambda (operand-tree env)
+      (make-environment '() operand-tree))))
+
+(define foreign-$define!
+  (make-foreign-operative
+    (lambda (operand-tree env)
+      (let* ((definiend (car operand-tree))
+             (expression (cadr operand-tree))
+             (expression-result (kernel-eval expression env))
+             (new-bindings (match-formal-parameter-tree definiend expression-result
+                             (environment-bindings env))))
+        (environment-bindings-set! env new-bindings)
+        +inert+))))
+
+(define foreign-operative?* (make-foreign-predicate operative?))
+(define foreign-applicative? (make-foreign-predicate applicative?))
+
+(define foreign-$vau
+  (make-foreign-operative
+    (lambda (operand-tree environment)
+      (let ((formals (car operand-tree))
+            (eformal (cadr operand-tree))
+            (expr (caddr operand-tree)))
+        (make-operative formals eformal expr environment)))))
+
+(define foreign-wrap
+  (make-foreign-applicative
+    (lambda (operand-tree environment)
+      (make-applicative (car operand-tree)))))
+
+(define foreign-unwrap
+  (make-foreign-applicative
+    (lambda (operand-tree env)
+      (applicative-combiner (car operand-tree)))))
+
+(define core-environment (make-environment `((boolean? . ,foreign-boolean?)
+                                             (equal? . ,foreign-equal?)
+                                             (symbol? . ,foreign-symbol?)
+                                             (inert? . ,foreign-inert?)
+                                             ($if . ,foreign-$if)
+                                             (pair? . ,foreign-pair?)
+                                             (null? . ,foreign-null?)
+                                             (cons . ,foreign-cons)
+                                             (environment? . ,foreign-environment?)
+                                             (ignore? . ,foreign-ignore?)
+                                             (eval . ,foreign-eval)
+                                             (make-environment . ,foreign-make-environment)
+                                             ($define! . ,foreign-$define!)
+                                             (operative? . ,foreign-operative?*)
+                                             (applicative? . ,foreign-applicative?)
+                                             ($vau . ,foreign-$vau)
+                                             (wrap . ,foreign-wrap)
+                                             (unwrap . ,foreign-unwrap)
+                                             )
+                                           '()))
+
+(define standard-environment (make-environment '() (list core-environment)))
+
+(define (kernel-repl)
+  (let ((exp (read)))
+    (unless (eof-object? exp)
+      (let ((exp (case exp ((+ignore+) +ignore+) ((+inert+) +inert+) (else exp))))
+        (write (kernel-eval exp standard-environment))
+        (newline)
+        (kernel-repl)))))
+
 
 ;; =====
 ;; TESTS
@@ -85,6 +206,7 @@
 (test-error "nil matching error" (match-formal-parameter-tree '() 42 '()))
 (test "matching pair" '((a . 42) (b . 21)) (match-formal-parameter-tree '(a . b) '(42 . 21) '()))
 (test "matching list" '((a . 42) (b . 21)) (match-formal-parameter-tree '(a b) '(42 21) '()))
+(test-error "duplicate symbol error" (match-formal-parameter '(a (a b)) '(1 (2 3)) '()))
 
 ) ; parameter matching group
 
@@ -117,5 +239,18 @@
 (test "evaluating simple operative" '(a b c) (kernel-eval '($list a b c) combiner-environment))
 (test "evaluating simple applicative" '(1 2 3) (kernel-eval '(list a b c) combiner-environment))
 
+
+(test-group "foreign combiners"
+
+(define foreign-environment (make-environment (list (cons '$vau foreign-$vau)
+                                                    (cons 'wrap foreign-wrap))
+                                              (list combiner-environment)))
+
+(test "foreign wrap call" '(1 2 3) (kernel-eval '((wrap $list) a b c) foreign-environment))
+(test "foreign $vau call" '(b a) (kernel-eval '(($vau (x y) #!ignore (list y x)) a b) foreign-environment))
+
+) ; foreign combiners group
+
 ) ; eval group
-(test-exit)
+
+(cond-expand (script (test-exit)) (else))

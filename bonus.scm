55d326484646f4cae5d6d80f7896816eabb4f17f
Kooda <kooda@upyum.com>
2020-08-16 02:18:56 +0200

    Some more core library

--- bonus.scm
+++ bonus.scm
@@ -1,3 +1,33 @@
+;; Numeric procedures
+
+(kernel-define! '+   (make-scheme-applicative +))
+(kernel-define! '-   (make-scheme-applicative -))
+(kernel-define! '>?  (make-scheme-applicative >))
+(kernel-define! '<?  (make-scheme-applicative <))
+(kernel-define! '>=? (make-scheme-applicative >=))
+(kernel-define! '<=? (make-scheme-applicative <=))
+(kernel-define! '=?  (make-scheme-applicative =))
+
+
+;; Encapsulations
+
+(kernel-define! 'make-encapsulation-type
+  (wrap (make-operative
+    (lambda (operands env)
+      (call-with-values
+        (lambda () (make-encapsulation-type))
+        (lambda res (map make-scheme-applicative res)))))))
+
+
+;; Temporary loading applicative
+
+(kernel-define! 'load
+  (wrap (make-operative (lambda (operands env)
+                          (kernel-load (car operands) env)))))
+
+
+;; Access to Scheme
+
 (kernel-define! '$scheme-eval
   (make-operative
     (lambda (operands env)
